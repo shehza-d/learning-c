d457586afb3adae3aefa450b1b08b17939734209
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2024-09-05 01:49:22 +0500

    cpp started

---
+++ cpp/01.setup/hello.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int myArr[5];
+
+	for (int i = 0; i < 5; i++)
+	{
+		cin >> myArr[i];
+	}
+      
+	cout << "Hello, test k Worl d!" << endl;
+	cout << myArr[1] << endl;
+	cout << myArr << endl; 
+	return 0;
+}
---
+++ cpp/README.md
@@ -0,0 +1,5 @@
+# Learning CPP
+
+[Strivers One Shot](https://youtu.be/EAR7De6Goz4?si=DHI5Rs5CGP4wGtuc)
+
+[Hitest's Playlist](https://youtube.com/playlist?list=PLu71SKxNbfoCPfgKZS8UE0MDuwiKvL8zi&si=-kFUih3MqVJG7t98)
